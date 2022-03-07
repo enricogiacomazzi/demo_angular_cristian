81e4655dafbbf19c6bb19f81d488ae6e584b777a
Enrico <e.giacomazzi@contralto-audio.com>
2022-03-07 18:05:26 +0100

    lezione 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,55 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'demo2';
+
+  public usernameRgx: RegExp = /^\w{4,}\d{2}$/; 
+
+  public formData: FormValue = {
+    name: '',
+    surname: 'rossi',
+    email: "ciao@prova.it",
+    username: 'pippo666',
+    gender: '',
+    business: false,
+    vatNumber: ''
+  }
+
+  public genders: Array<Gender> = [
+    {value: 'M', label: 'Maschio'},
+    {value: 'F', label: 'Femmina'},
+    {value: 'N', label: 'Non binario'},
+    {value: 'R', label: 'Robot'},
+  ]
+
+  
+  public save(form: NgForm): void {
+    if(form.valid) {
+      console.log('save', form);
+    } else {
+      for (const k in form.controls) {
+        form.controls[k].markAsTouched();
+      }
+    }
+  }
+
+  public change(): void {
+    this.formData.name = "pietro";
+  }
+
+
+}
+
+interface Gender {
+  value: string;
+  label: string;
+}
+
+interface FormValue {
+  name: string;
+  surname: string;
+  email: string;
+  username: string;
+  gender: string;
+  business: boolean;
+  vatNumber: string;
 }
